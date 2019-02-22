5d3a2342ff6e2ee547a635eef7e02cd9d15958a6
Wes Doyle <wesdoyle08@gmail.com>
2019-02-22 15:43:49 -0600

    Update client

--- abstract-factory/Client.cs
+++ abstract-factory/Client.cs
@@ -7,20 +7,22 @@ namespace abstract_factory
     /// </summary>
     internal class Client
     {
-        private readonly Human _human; 
-        private readonly Elf _elf;
+        private readonly Human _priya; 
+        private readonly Human _olaf; 
+        private readonly Elf _beleg;
 
         public Client(PlayerFactory playerFactory)
         {
-            _human = playerFactory.CreateHuman("Priya");
-            _elf = playerFactory.CreateElf("Beleg");
+            _priya = playerFactory.CreateHuman("Priya");
+            _olaf = playerFactory.CreateHuman("Olaf");
+            _beleg = playerFactory.CreateElf("Beleg");
         }
 
         internal void Run()
         {
-            _elf.Interact(_elf);
-            _human.Interact(_elf);
-            _human.Interact(_human);
+            _beleg.Interact(_olaf);
+            _priya.Interact(_olaf);
+            _olaf.Interact(_priya);
         }
     }
 }
